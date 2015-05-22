5ef2f024c70db20de0d407520fc3a67892febbb8
Elisabeth Anderson <beth@vintur.cc>
2015-05-22 13:33:03 +0100

    Initial

---
+++ src/launchpad.rb
@@ -0,0 +1,1 @@
+#!/usr/bin/env rubyrequire 'launchpad/device'p "Testing launchpad..."device = Launchpad::Device.newdevice.test_ledssleep 1device.resetsleep 1device.change :grid, :x => 4, :y => 4, :red => :high, :green => :low
